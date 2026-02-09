246b28fabfde3f9bf69b1f9b59af11b16b161ac8
Ben-TECHPlayer <158784500+Ben-TECHPlayer@users.noreply.github.com>
2026-02-09 19:19:52 +0100

    Mise à jour du site web

--- src/components/Annonce.js
+++ src/components/Annonce.js
@@ -2,8 +2,14 @@ function Annonce() {
 	return (
 		<section>
 			<h1>Coupe du monde 2026</h1>
-			<h2 className="info">Pour la toute première fois, la coupe du monde aura lieu dans 3 pays différents</h2>
-			<p>11 juin - 19 juillet</p>
+			<h2 className="info">Pour la toute première fois, la coupe du monde aura lieu dans 3 pays différents :       
+				<div className="image-item">
+					<img src={`${process.env.PUBLIC_URL}/assets/etats-unis.png`} alt="usa" />
+					<img src={`${process.env.PUBLIC_URL}/assets/canada.png`} alt="canada" />
+					<img src={`${process.env.PUBLIC_URL}/assets/mexique.png`} alt="mexique" />
+				</div>
+			</h2>
+			<p>Elle aura lieu entre le 11 juin et le 19 juillet 2026</p>
 		</section>
 	);
 }
--- src/components/Formulaire.js
+++ src/components/Formulaire.js
@@ -1,14 +1,60 @@
 function Formulaire() {
     return (
     <section>
-        <h2>Contactez-nous</h2>
         <form method="post">
+            <legend>Choisissez votre équipe !</legend>
             <input type="text" id="last-name" name="nom" placeholder="Votre nom" />
             <input type="text" id="first-name" name="prenom" placeholder="Votre prénom" />
             <input type="email" id="mail" name="adresse-mail" placeholder="Votre adresse-mail" />
+            <select name="pets" id="pet-select">
+                <option value="">Votre équipe préférée</option>
+                <option value="mexique">Mexique</option>
+                <option value="usa">Etats-Unis</option>
+                <option value="canada">Canada</option>
+                <option value="france">France</option>
+                <option value="germany">Allemagne</option>
+                <option value="south-korea">Corée du Sud</option>
+                <option value="spain">Espagne</option>
+                <option value="england">Angleterre</option>
+                <option value="argentina">Argentine</option>
+                <option value="brazil">Brésil</option>
+                <option value="switzerland">Suisse</option>
+                <option value="croatia">Croatie</option>
+                <option value="qatar">Qatar</option>
+                <option value="south-africa">Afrique du Sud</option>
+                <option value="scotland">Ecosse</option>
+                <option value="paraguay">Paraguay</option>
+                <option value="australia">Australie</option>
+                <option value="haitian">Haïti</option>
+                <option value="ecuador">Équateur</option>
+                <option value="ivory-coast">Côte d'Ivoire</option>
+                <option value="curacao">Curaçao</option>
+                <option value="belgium">Belgique</option>
+                <option value="egypt">Égypte</option>
+                <option value="netherlands">Pays-Bas</option>
+                <option value="portugal">Portugal</option>
+                <option value="saudi-arabia">Arabie Saoudite</option>
+                <option value="iran">Iran</option>
+                <option value="japan">Japon</option>
+                <option value="tunisia">Tunisie</option>
+                <option value="uruguay">Uruguay</option>
+                <option value="norway">Norvège</option>
+                <option value="poland">Pologne</option>
+                <option value="senegal">Sénégal</option>
+                <option value="algeria">Algérie</option>
+                <option value="colombia">Colombie</option>
+                <option value="jordanie">Jordanie</option>
+                <option value="new-zealand">Nouvelle-Zélande</option>
+                <option value="cape-verde">Cap-Vert</option>
+                <option value="ghana">Ghana</option>
+                <option value="panama">Panama</option>
+                <option value="austria">Autriche</option>
+            </select>
+            {/* tel: 0744271594 */}
             <textarea id="message" name="avis" rows="6" placeholder="Votre message"></textarea>
             <input type="submit" id="envoyer" name="validation" value="Envoyer"/>
         </form>
+        <p><i>Note: Missing data for qualifiedteams will be updated soon(after qualifying round).</i></p>
     </section>
     );
 }
